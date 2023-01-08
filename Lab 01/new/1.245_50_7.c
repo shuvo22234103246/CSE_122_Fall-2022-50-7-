95517997f6170cd4f4412b7a5447527dfc189f8f
nandanchakraborty <122105074+nandanchakraborty@users.noreply.github.com>
2023-01-08 14:54:52 +0600

    Add files via upload

---
+++ Lab 01/new/1.245_50_7.c
@@ -0,0 +1,22 @@
+//codeforces_problem_no_4A
+//problem_name - watermelon.
+
+
+
+
+#include<stdio.h>
+int main() {
+    int weight;
+    scanf("%d",&weight);
+    if (weight % 2 == 0 && weight > 2){
+       printf("YES\n");
+    }
+    else{
+
+        printf("NO\n");
+    }
+
+
+    return 0;
+}
+Click to see
