a53887bb59e0820cee2114a805085838da15cf4c
nandanchakraborty <nandancsebubt@gmail.com>
2023-01-16 22:34:05 +0600

    code add

--- lab02/codeforce_231.cpp
+++ lab02/codeforce_231.cpp
@@ -1,1 +1,24 @@
+//codeforce_problem_no_231A
+//problem_name_Team;
 
+
+
+#include<iostream>
+using namespace std;
+int main() {
+    int a,b,c,num,cou=0;
+    cin >> num;
+    for(int i =0; i<num; i++) {
+        cin >> a >> b >> c;
+        if((a == 1 && b == 1 ) || (b == 1 && c == 1) || ( a == 1 && c == 1)) {
+            cou += 1;
+        }
+
+    }
+
+
+    cout << cou;
+
+
+
+}
