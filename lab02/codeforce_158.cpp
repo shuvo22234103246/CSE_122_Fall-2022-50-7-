13c3e4da2bddc8ddf4db9bbcc01c352c5a47290e
nandanchakraborty <nandancsebubt@gmail.com>
2023-01-16 20:56:09 +0600

    code reformatted

--- lab02/codeforce_158.cpp
+++ lab02/codeforce_158.cpp
@@ -2,23 +2,19 @@
 //problem_name_next_round;
 
 
-
 #include<iostream>
 using namespace std;
-int main()
-{
-int n,k,a[52],cou=0;
- cin >> n >> k;
- for(int i =0;i<n;i++)
- {
- cin >> a[i];
-  }
-for(int i =0;i<n;i++)
- {
+int main() {
+    int n,k,a[52],cou=0;
+    cin >> n >> k;
+    for(int i =0; i<n; i++) {
+        cin >> a[i];
+    }
+    for(int i =0; i<n; i++) {
         if(a[i]>=a[k-1]&&a[i]>0)
             cou++;
     }
-cout << cou;
+    cout << cou;
 
 
 }
