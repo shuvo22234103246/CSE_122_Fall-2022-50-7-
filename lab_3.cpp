4322d2dc61516fa11041b823b316afc6d3fd1a05
Nandan Chakraborty <122105074+nandanchakraborty@users.noreply.github.com>
2023-01-23 22:07:31 +0600

    Add files via upload

---
+++ lab_3.cpp
@@ -0,0 +1,20 @@
+//codeforce_pr0blem_50A
+//problem_name__-Domino_piling
+
+
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main()
+{
+
+
+    int M, N;
+    cin >> M >> N;
+    cout << M * N / 2 << endl;
+    return 0;
+
+
+    return 0;
+
+}
