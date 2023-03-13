dac880d8d33030ec67d746a4750199e35c6843e7
Nandan Chakraborty <122105074+nandanchakraborty@users.noreply.github.com>
2023-03-13 18:25:18 +0600

    Add files via upload

---
+++ lab7/bear_and_big_brother.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+   int a,b,res = 1,sum = 1,i;
+    cin>>a>>b;
+    for(i =1;i<=10;i++)
+    {
+        a = a*3;
+        b = b*2;
+        if(a>b)
+
+    {
+       cout<<i;
+        break;
+
+    }
+
+
+
+    }
+
+    return 0;
+}
